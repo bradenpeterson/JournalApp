082c1b6f13b57f03123db92ffe21850da538bc93
Braden Peterson <peterson.braden2@gmail.com>
2025-11-25 11:39:21 -0700

    Wrote views

--- _server/core/views.py
+++ _server/core/views.py
@@ -3,6 +3,9 @@ from django.conf  import settings
 import json
 import os
 from django.contrib.auth.decorators import login_required
+from rest_framework import viewsets, permissions, filters
+from .models import JournalEntry, Tag
+from .serializers import JournalEntrySerializer, TagSerializer
 
 # Load manifest when server launches
 MANIFEST = {}
@@ -21,3 +24,31 @@ def index(req):
         "css_file": "" if settings.DEBUG else MANIFEST["src/main.ts"]["css"][0]
     }
     return render(req, "core/index.html", context)
+
+
+class JournalEntryViewSet(viewsets.ModelViewSet):
+    serializer_class = JournalEntrySerializer
+    permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['title', 'content', 'mood', 'tags__name']
+    ordering_fields = ['date', 'created_at', 'updated_at']
+
+    def get_queryset(self):
+        return JournalEntry.objects.filter(user=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+    
+
+class TagViewSet(viewsets.ModelViewSet):
+    serializer_class = TagSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['name']
+    ordering_fields = ['name']
+
+    def get_queryset(self):
+        return Tag.objects.filter(user=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
